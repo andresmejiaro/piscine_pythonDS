9b8c2cb8c16b47f1e4396605247f7c9a0a419d76
andresmejiaro <andresmejiaro@gmail.com>
2024-03-04 13:12:55 +0100

    leaving

---
+++ day1/ex08/Loading.py
@@ -0,0 +1,21 @@
+#%%
+
+class ft_tqdm():
+    def __init__(self, x: object):
+        self.internal_iterator = iter(x)
+        self.current = 0
+        self.max = len(x)
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        perProgress = self.current/self.max
+        perProgressRemain= 1- perProgress
+        consoleWidth = 166
+        
+        print(f"\r{int(100*perProgress)}%|{int(perProgress*consoleWidth)*"█"}"
+            + f"{int(perProgressRemain*consoleWidth)*" "}|" +
+            f" {self.current}/{self.max}" , end="")
+        self.current += 1
+        return next(self.internal_iterator)
